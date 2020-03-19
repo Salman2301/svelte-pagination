5c16b74ece1dc24e3b208ca6f7a99508f5ae2d0f
salman2301 <salmanhammed77@gmail.com>
2020-03-19 11:04:41 +0530

    first working prototype

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Pagination from "./Pagination.svelte";
+	let ttlPage = 10;
+	
+	const handleCurrPage = e =>{
+		console.log("currPage :", e.detail );
+	}
+</script>
+
+<!-- Test section -->
+<button on:click={()=>ttlPage++}>
+	Add MORE PAGE	
+</button>
+
+<!-- Test section end-->
+
+<Pagination ttlPage={ttlPage} on:currPage={handleCurrPage} />
---
+++ src/Pagination.svelte
@@ -0,0 +1,163 @@
+<script>
+	import {createEventDispatcher} from "svelte"; 
+	
+	let dispatch = createEventDispatcher();
+	
+	export let startPage = 1;
+	export let ttlPage = 10;
+	export let maxPage = 5;
+	export let currPage = startPage;
+
+	$: endPage = startPage + ttlPage -1;
+	$:  arr =	buildArr(startPage, ttlPage, endPage);
+
+	// dispatch on current page change
+	$: dispatch("currPage", currPage );
+	
+
+
+	let forwardArrow = ">";
+	let backwardArrow = "<";
+	let fastForwardArrow = ">>";
+	let fastBackwardArrow = "<<";
+	
+	function buildArr() {	
+		// console.log({startPage, endPage, maxPage, ttlPage});
+		let tempArr  = [];
+		for(
+				let i =startPage; 
+				(i <= endPage) && (tempArr.length < maxPage);
+				i++
+			 ) {
+				let obj = {
+					id:String(i),
+					label: i
+				}
+				tempArr = [...tempArr, obj];
+		 }
+	
+		return tempArr;
+	}
+	
+	// increment setting
+	function incPage() {
+		if(ttlPage === currPage) return;
+		if(currPage === startPage+maxPage-1) startPage++;
+		currPage++;
+	}
+
+	function incSlot() {
+		if(ttlPage===currPage) return;
+		
+		let newPage = currPage;
+		newPage +=5;
+		if( newPage > ttlPage ) newPage = ttlPage;
+
+		currPage = newPage;
+		
+		if(
+			currPage < startPage+maxPage + 4
+			&& startPage+maxPage + 4 <= ttlPage
+		) {
+			startPage+=5;
+			if(startPage + maxPage > ttlPage)	startPage = ttlPage - maxPage + 1;
+		}
+	}
+	
+	// decrement setting
+	function decPage() {
+		if(currPage === 1) return;
+		if(currPage === startPage) startPage --;
+		currPage--;
+	}
+	
+	function decSlot() {
+		if(currPage===1) return;
+		
+		let newPage = currPage;
+		newPage -= 5;
+		if( newPage < 1 ) newPage=1;
+
+		currPage = newPage;
+		
+		if( currPage < startPage ) {
+			startPage -= 5;
+			if(startPage < 1) startPage = 1;
+		}
+	}
+	
+</script>
+
+
+<div class="pagination" >
+	
+	<p class="btn backward"  on:click={decSlot} >
+		{fastBackwardArrow}
+	</p >
+	
+	<p class="btn backward" on:click={decPage}>
+		{backwardArrow}
+	</p>
+	
+	<div class="btns">
+		{#each arr as el,  i}
+			<p 
+				 class="btn" 
+				 class:active={currPage === el.label}
+				 on:click={()=>currPage = el.label}
+				 >
+				{el.label}
+			</p>
+		{/each}
+	</div>
+	
+	
+	<p class="btn forward"  on:click={incPage} >
+		{forwardArrow}
+	</p >
+	<p class="btn forward"  on:click={incSlot} >
+		{fastForwardArrow}
+	</p >
+	
+</div>
+
+<style>
+	.pagination {
+		display: flex;
+		width: 400px;
+		
+	}
+	.pagination > .btns {
+			justify-content: center;
+			width:60%;
+	}
+	
+	.btn {
+		border : 1px solid black;
+		border-radius: 100%;
+		width: 30px;
+		height: 30px;
+		text-align: center;
+		margin: 0px 5px;
+		cursor: pointer;
+		padding-top: 2px;
+	}
+	
+	.btns {
+		display:flex;
+		width: 50%;
+		justify-content: center;
+	}
+	
+	.active {
+		background: #eee;
+	}
+	
+	.forward, .backward {
+		background: #eee;
+	}
+	
+	.btn:hover {
+		background: #eee;
+	}
+</style>
